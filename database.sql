39aed6e5469bc525fee09eebb8431c69a33a9be0
akaespiritosanto <2097624@student.uma.pt>
2025-04-20 21:50:24 +0100

    20/04/2025
    sfsd

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE mini_projeto;
 
-CREATE TABLE mini_projeto.product(
+CREATE TABLE mini_projeto.products(
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     barcode VARCHAR(30) NOT NULL,
@@ -9,16 +9,15 @@ CREATE TABLE mini_projeto.product(
     description VARCHAR(3000) NOT NULL,
     weight INT NOT NULL,
     price INT NOT NULL,
+    date DATE NOT NULL,
     created TIME NULL,
     comment VARCHAR(3000) NULL
 );
 
-INSERT INTO mini_projeto.product (id, name, barcode, department, review, description, weight, price, created, comment)
-VALUES (1, 'Smartphone XYZ', '123456789012', 'Eletrônicos', 4, 'Smartphone avançado com tela de 6.5 polegadas, 128GB de armazenamento e câmera tripla.', 180, 899, '09:30:00', '{"user":"João", "comment":"Bom produto, mas a bateria poderia durar mais."}'),
-    (2, 'Notebook ABC', '987654321098', 'Eletrônicos', 5, 'Notebook potente com processador i7, 16GB RAM e SSD de 512GB.', 2200, 1499, '10:15:00', '{"user":"Maria", "comment":"Performance excelente, superou minhas expectativas!"}'),
-    (3, 'Garrafa Térmica', '456123789045', 'Casa e Cozinha', 3, 'Garrafa térmica de aço inoxidável com capacidade para 500ml.', 350, 29, '11:20:00', NULL),
-    (4, 'Fones de Ouvido Bluetooth', '789012345678', 'Eletrônicos', 4, 'Fones sem fio com cancelamento de ruído e bateria de longa duração.', 150, 129, '14:45:00', '{"user":"Carlos", "comment":"Ótimo som, muito confortáveis."}'),
-    (5, 'Livro - Aventuras SQL', '321654987012', 'Livros', 5, 'Guia completo para dominar consultas SQL e otimização de bancos de dados.', 500, 45, '16:00:00', NULL);
+INSERT INTO mini_projeto.products (id, name, barcode, department, review, description, weight, price, date, created, comment)
+VALUES (1, 'Iphone', '123456789012', 'Eletrónicos', 4, 'Smartphone avançado com ecran de 6.5 polegadas, 128GB de armazenamento e câmera tripla.', 180, 899, '2025-03-15', '09:30:00', '{"user":"João", "comment":"Bom produto, mas a bateria poderia durar mais."}'),
+    (2, 'Portátil', '987654321098', 'Eletrónicos', 5, 'Portátil potente com processador i7, 16GB RAM e SSD de 512GB.', 2200, 1499, '2025-07-22', '10:15:00', '{"user":"Maria", "comment":"Performance excelente, superou as minhas expectativas!"}'),
+    (3, 'Garrafa-Térmica', '456123789045', 'Casa-e-Cozinha', 3, 'Garrafa térmica de aço inoxidável com capacidade para 500ml.', 350, 29, '2025-05-01', '11:20:00', NULL),
+    (4, 'Fones-de-Ouvido Bluetooth', '789012345678', 'Eletrónicos', 4, 'Fones sem fio com cancelamento de ruído e bateria de longa duração.', 150, 129,'2025-11-30', '14:45:00', '{"user":"Carlos", "comment":"Ótimo som, muito confortáveis."}'),
+    (5, 'Livro-Aventuras-SQL', '321654987012', 'Livros', 5, 'Guia completo para dominar consultas SQL e otimização de bases de dados.', 500, 45, '2025-09-14', '16:00:00', NULL);
     
-USE mini_projeto;
-SELECT * FROM product
--- server.js
+++ server.js
@@ -2,31 +2,293 @@ const express = require('express');
 const mysql = require('mysql');
 const nodemon = require('nodemon');
 const app = express();
-const port = 3001;
+const port = 3000;
+app.use(express.json());
 
 const connection = mysql.createConnection({
   host:"localhost",
   user:"root",
   password:"password",
-  database:"mini_projeto",
-})
-
-app.get('/test', (req, res) =>{
-  res.send('It is Working!');
+  database:"mini_projeto"
 });
 
 // PARTE A
 
-app.get("/users", (req, res) =>{
-  connection.query("", (err, results, fields){
-    if (err){
-      console.log(err);
-    }
-    else{
-      res.send(results);
-    }
-  })
+// ============================================================================================================== (a)
+
+app.get("/products-list", (req, res) =>{
+  connection.query("SELECT id, name FROM products", function(error, results, fields){
+    if (error) {
+      res.status(500).send("Server error");
+      throw error;
+    };
+    if (results.length == 0){
+      res.status(404).send("No products found.");
+    };
+    res.send(results);
+  });
 });
+
+// Example: localhost:3000/products-list
+
+// ============================================================================================================== (b)
+
+app.post("/products-new", (req, res) =>{
+  connection.query("SELECT MAX(id) as max_id, CURRENT_DATE() as date, CURRENT_TIME as time FROM products", function(error, results, fields){
+    if (error) {
+      res.status(500).send("Server error");
+      throw error;
+    };
+    var max_id = results[0].max_id;
+    var new_id = max_id + 1;
+    var date = results[0].date;
+    var time = results[0].time;
+    var data = [
+      new_id,
+      'Teclado mecânico',
+      '314236377956',
+      'Eletrónicos',
+      4,
+      'Teclado gaming com anti-ghosting e rapid trigger',
+      650,
+      80,
+      date,
+      time,
+      '{"user": "Ana", "comment": "Produto excelente!"}',
+      'in stock',
+      15,
+      'TechSupplier'
+    ];
+    connection.query("INSERT INTO products (id, name, barcode, department, review, description, weight, price, date, created, comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data,
+      function(error, results, fields){
+        if (error) {
+          res.status(500).send("Server error");
+          throw error;
+        }
+        res.send(`Produto inserido, id do produto: ${new_id}`)
+      });
+  });
+});
+
+// Example: localhost:3000/products-new
+
+// ============================================================================================================== (c)
+
+app.get("/products/:department", (req, res) =>{
+  var department = req.params.department;
+  connection.query("SELECT name FROM products WHERE department = ?", department, function(error, results, fields){
+    if (error){
+      res.status(500).send("Server error");
+      throw error;
+    };
+    res.send(results);
+  });
+});
+
+// Example: localhost:3000/products/eletronicos
+
+// ============================================================================================================== (d)
+
+app.patch("/products-discount", (req, res) =>{
+  var name = req.query.name;
+  var discount = req.query.discount / 100;
+  connection.query("SELECT price FROM products WHERE name = ?", name, function(error, results, fields){
+    if (error){
+      res.status(500).send("Server error");
+      throw error;
+    };
+    if (results.length == 0){
+      res.status(404).send("Product wasn't found.");
+    };
+    var final_price = results[0].price - (results[0].price * discount)
+    var params = [final_price, name]
+    connection.query("UPDATE products SET price = ? WHERE name = ?", params, function(error, results, fields){
+      if (error){
+        res.status(500).send("Server error");
+        throw error;
+      };
+      connection.query("SELECT name, price FROM products WHERE name = ?", name, function(error, results, fields){
+        if (error){
+          res.status(500).send("Server error");
+          throw error;
+        };
+        if (results.length == 0){
+          res.status(404).send("Price not updated, try again.");
+        };
+        res.send("Price updated with sucess!")
+      });
+    });
+  });
+});
+
+// Example: localhost:3000/products?name=iphone&discount=20
+
+// ============================================================================================================== (e)
+
+app.get("/products-date/:date", (req, res) =>{
+  var date = req.params.date;
+  connection.query("SELECT id, name, date FROM products WHERE date < ?", date, function(error, results, fields){
+    if (error){
+      res.status(500).send("Server error");
+      throw error;
+    };
+    if (results.length == 0){
+      res.status(404).send("Products created before this date weren't found.");
+    };
+    res.send(results);
+  });
+});
+
+// Example: localhost:3000/products/2025-03-16
+
+// ==============================================================================================================
+
+// PARTE B
+
+// ============================================================================================================== (a)
+
+app.get("/products-id/:id", (req, res) => {
+  const id = req.params.id;
+  try {
+    connection.query("SELECT * FROM products WHERE id=?", [id], (err, rows) => {
+      if (rows.length === 0) {
+        return res.status(404).json({ message: "Product with id " + id + " not found!!" });
+      }
+      const product = rows[0];
+      res.status(200).json({
+        id: product.id,
+        name: product.name,
+        barcode: product.barcode,
+        department: product.department,
+        description: product.description,
+        review: product.review,
+        weight: product.weight,
+        price: product.price,
+        comment: product.comment,
+      });
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Erro no server!!!" });
+  }
+});
+
+// ============================================================================================================== (b)
+
+app.delete('/products/:id', (req, res) =>{
+  const id = req.params.id;
+  try {
+    connection.query("SELECT * FROM products WHERE id=?", [id], (err, rows) => {
+      if (!(rows.length === 0)) { 
+        try {
+          connection.query("DELETE FROM products WHERE id=?", [id], (err, rows) => {
+            res.status(200).json({ message: "Product with id " + id + " was deleted!!" });
+          });
+        } catch (err) {
+          res.status(500).json({ message: "Erro no server!!!" });
+        }
+      }else{
+        return res.status(404).json({ message: "Product with id " + id + " not found!!" });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Erro no server!!!" });
+  }
+});
+
+// ============================================================================================================== (c)
+
+app.post('/products-search', (req, res) => {
+  const keyWord = req.body.word;
+  var guardarId = [];
+  try {
+      connection.query("SELECT id, description FROM products", (err, results) => {
+          if (err) {
+              return res.status(500).json({ message: "Error retrieving products." });
+          }
+          for (let index = 0; index < results.length; index++) {
+              var element = results[index].description.toLowerCase();
+              console.log(element);
+              if (element.includes(keyWord.toLowerCase())) { 
+                  guardarId.push(results[index].id);
+              }
+          }
+          const placeholders = guardarId.map(() => '?').join(',');
+
+          connection.query(`SELECT * FROM products WHERE id IN (${placeholders})`, guardarId, (err, result) => {
+              if (err) {
+                  return res.status(500).json({ message: "Error retrieving products by ID." });
+              }
+              res.send(result);
+              console.log(result);
+          });
+      });
+  } catch (err) {
+      res.status(500).json({ message: "Error in server!!!" });
+  }
+});
+
+// ============================================================================================================== (d)
+
+app.patch("/products-comment/:id", (req, res) =>{
+  var id = req.params.id
+  connection.query("SELECT id, name, comment FROM products WHERE id = ?", id, function(error, results, fields){
+    if (error){
+      res.status(500).send("Server error");
+      throw error;
+    };
+    if (results.length == 0){
+      res.status(404).send("Product wasn't found.");
+    };
+    var user = "Maria"
+    var comment = "Nada a apontar"
+    var new_comment_obj = {
+      user: user,
+      comment: comment
+    }
+    var new_comment = `{"user":"${new_comment_obj.user}", "comment":"${new_comment_obj.comment}"}`
+    var all_comment = results[0].comment + "," + new_comment
+    var data = [all_comment, id]
+    connection.query("UPDATE products SET comment = ? WHERE id = ?", data, function(error, results, fields){
+      if (error){
+        res.status(500).send("Server error");
+        throw error;
+      };
+      if (results.length == 0){
+        res.status(404).send("Product comments weren't updated, try again.");
+      };
+      connection.query("SELECT id, name, comment FROM products WHERE id = ?", id, function(error, results, fields){
+        if (error){
+          res.status(500).send("Server error");
+          throw error;
+        };
+        if (results.length == 0){
+          res.status(404).send("Product wasn't found.");
+        };
+        res.send(results);
+      });
+    });
+  });
+});
+
+// Example: localhost:3000/products/1
+
+// ============================================================================================================== (e)
+
+app.get("/products-ascending", (req, res) =>{
+  connection.query("SELECT id, name, price FROM products", function(error, results, fields){
+    if (error){
+      res.status(500).send("Server error");
+      throw error;
+    };
+    if (results.length == 0){
+      res.status(404).send("Products not found.");
+    };
+    results.sort((a,b) => a.price - b.price);
+    res.send(results);
+  });
+});
+
+// ==============================================================================================================
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
